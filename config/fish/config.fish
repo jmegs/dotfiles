a1f8a7b9abd2f8da960432d734f500263b84d69a
John Meguerian <johnmeguerian@gmail.com>
2023-02-24 10:43:55 -0500

    add pnpm and update deps

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,15 @@
-# add homebrew things to the actual $PATH
-fish_add_path -P /opt/homebrew/bin /opt/homebrew/sbin
-
-# append ~/.bin to the end of the fish_user_paths
-fish_add_path --append ~/.bin
-
 set -g fish_greeting # remove welcome message
-
 set -gx GOPATH ~/code/go
 set -gx HOMEBREW_BUNDLE_NO_LOCK 1
 set -gx NODEJS_CHECK_SIGNATURES no
-set -gx hydro_two_line_prompt true
+set -gx hydro_multiline true
+
+# add homebrew things to the actual $PATH
+fish_add_path -P /opt/homebrew/bin /opt/homebrew/sbin
+fish_add_path -P $GOPATH/bin
+
+# append ~/.bin to the end of the fish_user_paths
+fish_add_path --append ~/.bin
 
 # Path aliases
 alias ..="cd ../"
@@ -26,3 +26,8 @@ alias g="git"
 
 # source in asdf from homebrew
 source (brew --prefix asdf)/libexec/asdf.fish
+
+# pnpm
+set -gx PNPM_HOME "/Users/john/Library/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
