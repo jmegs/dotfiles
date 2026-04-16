7b12b818fba9f5073cce0064fe19f638a33fbd93
John Meguerian <johnmeguerian@gmail.com>
2026-04-16 09:52:21 -0400

    Update colorscheme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -3,15 +3,8 @@ return {
     "webhooked/kanso.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      background = {
-        dark = "ink",
-        light = "pearl",
-      },
-    },
   },
 
-  -- configure lazyvim to load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
