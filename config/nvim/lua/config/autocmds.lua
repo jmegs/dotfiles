10904abf7b218342ab269ba07f3a7180496a8484
John Meguerian <johnmeguerian@gmail.com>
2026-04-16 09:52:29 -0400

    Disable autocompletion in markdown files

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -3,9 +3,11 @@
 -- Add any additional autocmds here
 
 -- Disable diagnostics by default in markdown files (<leader>ud to toggle back on)
+-- Disable autocomplete in markdown files
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown" },
   callback = function(ev)
     vim.diagnostic.enable(false, { bufnr = ev.buf })
+    vim.b[ev.buf].completion = false
   end,
 })
