d53ff489017afe36de44febbb553eba86ecb7366
John Meguerian <johnmeguerian@gmail.com>
2026-04-24 13:40:11 -0400

    Switch colorscheme to Kanso

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,14 @@
 return {
   {
-    "loctvl842/monokai-pro.nvim",
+    "webhooked/kanso.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("monokai-pro").setup({
-        filter = "pro",
-      })
-    end,
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "monokai-pro",
+      colorscheme = "kanso",
     },
   },
 }
